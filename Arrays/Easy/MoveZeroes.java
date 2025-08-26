a7b56b0b10af3aedbe9d5d7e604f85aba4a69ff7
krharsh-code <kumarjhapradeep352@gmail.com>
2025-08-26 12:27:54 +0530

    Add Arrays Easy problems

---
+++ Arrays/Easy/MoveZeroes.java
@@ -0,0 +1,17 @@
+// Problem: Move Zeroes
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
