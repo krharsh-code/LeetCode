ba57b031a14676a8fa4c178ddf60213cab369e9f
Harsh <hk5497697@gmail.com>
2026-03-19 11:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (82.81%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -1,18 +1,36 @@
- 
-class Solution {
-    public List<Integer> postorder(Node root) {
-        List<Integer> result = new ArrayList<>();
-        helper(root, result);
-        return result;
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
     }
 
-    private void helper(Node root, List<Integer> result) {
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(Node root, List<Integer> res) {
         if (root == null) return;
 
         for (Node child : root.children) {
-            helper(child, result);
+            dfs(child, res);
         }
 
-        result.add(root.val); // Root last
+        res.add(root.val); // add AFTER children
     }
 }
