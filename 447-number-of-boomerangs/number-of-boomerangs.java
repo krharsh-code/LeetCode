c1bc2d55062c482d958dd6db24ef8e771fc0587b
Harsh <hk5497697@gmail.com>
2026-03-31 12:37:11 +0530

    Time: 63 ms (97.45%) | Memory: 47.1 MB (11.29%) - LeetSync

--- 447-number-of-boomerangs/number-of-boomerangs.java
+++ 447-number-of-boomerangs/number-of-boomerangs.java
@@ -1,22 +1,17 @@
-class Solution {
+public class Solution {
     public int numberOfBoomerangs(int[][] points) {
-        int n = points.length;
-        if(n<3)return 0;
-        int count =0;
-        for(int i=0;i<n;i++){
-            HashMap<Long,Integer> hm= new HashMap<>();
-            for(int j=0;j<n;j++){
-                if(j==i)continue;
-                int y = points[i][1]-points[j][1];
-                int x = points[i][0]-points[j][0];
-                long key = y*y + x*x;
-                hm.put(key,hm.getOrDefault(key,0)+1);
+        int result = 0;
+        HashMap<Integer,Integer> distMap = new HashMap<Integer,Integer>();
+        for(int[] i : points) {
+            for(int[] j : points) {
+                if(i==j) continue;
+                int dist = (i[0]-j[0])*(i[0]-j[0]) + (i[1]-j[1])*(i[1]-j[1]);
+                int prevDist = distMap.containsKey(dist) ? distMap.get(dist) : 0;
+                result += 2*prevDist;
+                distMap.put(dist, prevDist+1);
             }
-            for(Map.Entry<Long,Integer> u : hm.entrySet()){
-                int k = u.getValue();
-                if(k>1) count+= k*(k-1);
-            }
+            distMap.clear();
         }
-        return count;
+        return result;
     }
 }
